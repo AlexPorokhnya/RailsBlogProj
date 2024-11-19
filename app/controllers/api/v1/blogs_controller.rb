1aa1fdb244e8c13b38efbc2d2649b76138780004
AlexPorokhnya <porohnyasasha75@gmail.com>
2024-11-19 20:20:11 +0200

    Create a blog controller

---
+++ app/controllers/api/v1/blogs_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class BlogsController < ApplicationController
+      def index
+
+      end
+
+      def create
+
+      end
+
+      def show
+
+      end
+
+      def update
+
+      end
+
+      def destroy
+
+      end
+    end
+  end
+end
